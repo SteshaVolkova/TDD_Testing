2c3d7259da93910f0053afa3215bcb6bd4ae07ba
SteshaVolkova <stepashechka5@gmail.com>
2026-05-26 19:02:39 +0300

    Work through all the described tests.

--- src/validate-password.test.js
+++ src/validate-password.test.js
@@ -1,13 +1,37 @@
 import { validatePassword } from "./validate-password";
 
 describe('validatePassword', () => {
-    it.tobe('should return true for a valid password');
+    it('should return true for a valid password', () => {
+        const validPassword = 'myPassword123!';
 
-    it.todo('should validate a password against 8 minimum characters');
+        expect(validatePassword(validPassword)).toBe(true);
+    });
 
-    it.todo('should validate a password against mixed case');
+    it('should validate a password against 8 minimum characters', () => {
+        const invalidPassword = 'Pass67!';
 
-    it.todo('should validate a password against digits and characters');
+        expect(validatePassword(invalidPassword)).toBe(false);
+    });
 
-    it.todo('should validate a password againsial character');
+    it('should validate a password against mixed case', () => {
+        const invalidPassword = 'mypassword123!';
+        const invalidPassword2 = 'MYPASSWORD123!';
+
+        expect(validatePassword(invalidPassword)).toBe(false);
+        expect(validatePassword(invalidPassword2)).toBe(false);
+    });
+
+    it('should validate a password against digits and characters', () => {
+        const invalidPassword = 'myPassword@#!';
+        const invalidPassword2 = 'myPassword&**';
+
+        expect(validatePassword(invalidPassword)).toBe(false);
+        expect(validatePassword(invalidPassword2)).toBe(false);
+    });
+
+    it('should validate a password agains special character', () => {
+        const invalidPassword = 'MyPassword123';
+
+        expect(validatePassword(invalidPassword)).toBe(false);
+    });
 });
