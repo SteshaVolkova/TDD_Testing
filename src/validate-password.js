6ee68c50796848036c0cd9d323d7a9ca6957c4e5
SteshaVolkova <stepashechka5@gmail.com>
2026-05-26 19:18:57 +0300

    Writing a password validation function that passes all tests.

--- src/validate-password.js
+++ src/validate-password.js
@@ -1,3 +1,19 @@
 export function validatePassword(password) {
-    
+    if (password.length < 8) {
+        return false;
+    }
+
+    if (!/[A-Z]/.test(password) || !/[a-z]/.test(password)) {
+        return false;
+    }
+
+    if (!/[0-9]/.test(password)) {
+        return false;
+    }
+
+    if(!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        return false;
+    }
+
+    return true;
 }
