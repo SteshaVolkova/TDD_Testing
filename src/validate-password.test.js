f9eba7831f60a94f6ed587682b95bccb0fb25a9a
SteshaVolkova <stepashechka5@gmail.com>
2026-05-26 18:21:30 +0300

    add test todos

---
+++ src/validate-password.test.js
@@ -0,0 +1,13 @@
+import { validatePassword } from "./validate-password";
+
+describe('validatePassword', () => {
+    it.tobe('should return true for a valid password');
+
+    it.todo('should validate a password against 8 minimum characters');
+
+    it.todo('should validate a password against mixed case');
+
+    it.todo('should validate a password against digits and characters');
+
+    it.todo('should validate a password againsial character');
+});
